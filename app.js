430a9f578d272c1208354b01c22e64b909c8f966
JimmyHuang3364 <open3364@hotmail.com>
2021-09-28 14:39:07 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+// 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const restaurantsList = require('./restaurants.json')
 const port = 3000
+
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })  //連線資料庫
+//取得資料庫連線狀態
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!!')
+})
+
 
 app.engine('handlebars', exphbs({ defaultlayout: 'main' }))
 app.set('view engine', 'handlebars')
