0acb1ae61d6aa6ff69aeb3d4ee6c3167866e51ad
JimmyHuang3364 <open3364@hotmail.com>
2021-09-30 21:17:23 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
-const Restaurant = require('./models/restaurant')
 const port = 3000
 
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })  //連線資料庫
-//取得資料庫連線狀態
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!!')
-})
+// 連線資料庫
+require('./config/mongoose')
 
 
 app.engine('hbs', exphbs({ defaultlayout: 'main' }))
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,13 +1,7 @@
-const mongoose = require('mongoose')
 const restaurant = require('../restaurant')
 const restaurantsList = require('./restaurants.json')
 
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })  //連線資料庫
-//取得資料庫連線狀態
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!!')
-})
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
   console.log('mongodb connected!!')
