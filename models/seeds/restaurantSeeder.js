040a64dedf8c671486c30b75f1891e6970873b93
JimmyHuang3364 <open3364@hotmail.com>
2021-10-23 09:20:34 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -4,52 +4,37 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const RESTAURANT = require('../restaurant')
 const User = require('../user')
-const restaurantsList = require('./restaurants.json')
-const SEED_USER = require('./users.json')
+const restaurantsList = require('./restaurants.json').results
+const SEED_USER = require('./users.json').results
 const db = require('../../config/mongoose')
 
 
 db.once('open', () => {
-  SEED_USER.results.forEach(element => {
-    bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(element.password, salt))
-      .then(hash => User.create({
-        mame: element.name,
-        email: element.email,
-        password: hash
-      }))
-      .then(user => {
-        const userId = user._id
-        const userFavorite = []
-        element.favoriteReataurantID.forEach(idNum => {
-          userFavorite.push(restaurantsList.results.find((item) => {
-            return item.id === idNum
-          }))
-        })
-
-
-        Array.from(userFavorite, restaurant => {
-          RESTAURANT.create({
-            name: restaurant.name,
-            name_en: restaurant.name_en,
-            category: restaurant.category,
-            image: restaurant.image,
-            location: restaurant.location,
-            phone: restaurant.phone,
-            google_map: restaurant.google_map,
-            rating: restaurant.rating,
-            description: restaurant.description,
-            userId: userId
+  console.log('加載種子資料')
+  Promise.all(
+    Array.from(SEED_USER, element => {
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(element.password, salt))
+        .then(hash => User.create({
+          mame: element.name,
+          email: element.email,
+          password: hash
+        }))
+        .then(user => {
+          const userId = user._id
+          const userFavorite = []
+          Array.from(element.favoriteReataurantIdex, index => {
+            restaurantsList[index].userId = userId
+            userFavorite.push(restaurantsList[index])
           })
+          return RESTAURANT.create(userFavorite)
         })
-      })
-    // .then(() => {
-    //   console.log('done.')
-    //   process.exit()
-    // })
-  })
-
-  console.log('種子資料載入完成!!')
-  console.log('按 Ctrl + C 退出')
+    }))
+    .then(() => {
+      console.log('加載完成')
+      console.log('退出程式')
+      process.exit()
+    })
+    .catch(err => console.log(err))
 })
